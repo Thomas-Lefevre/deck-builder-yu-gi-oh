9ca7ab00caac0549d022b1cabb834da2bbf16a72
Deleau.E <edeleau@hmail.com>
2020-05-26 14:08:01 +0200

    Ajout du workbench + début du home

--- public/js/home.js
+++ public/js/home.js
@@ -2,12 +2,12 @@ var callBackGetSuccess = function(data){
     console.log("donnees api", data);
     // var element = document.getElementById("zone_meteo");
     // element.innerHTML = "info de la carte :"+data.atk
-    alert("attaque :" + data.atk)
+    console.log("attaque :" + JSON.stringify(data.data[0].atk) );
 }
 
 
 function buttonClickGET(){
-    var url="https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician"
+    var url="https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark%20Magician"
 
     $.get(url, callBackGetSuccess).done(function(){
         //alert("second success");
