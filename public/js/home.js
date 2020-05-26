22767d18aaf0dc651c47019fca7023a7cc8701a1
Thomas-Lefevre <thomas.lefevre4652@gmail.com>
2020-05-26 09:52:50 +0200

    recherche de carte par nom dans l'api

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,14 @@
 var callBackGetSuccess = function(data){
     console.log("donnees api", data);
-    // var element = document.getElementById("zone_meteo");
-    // element.innerHTML = "info de la carte :"+data.atk
-    alert("attaque :" + data.atk)
+    var element = document.getElementById("zone_carte");
+    element.innerHTML = "description de la carte :"+data.data[0].desc;
+    // alert("description :" + data.desc)
 }
 
 
 function buttonClickGET(){
-    var url="https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician"
+    var queryLoc = document.getElementById("queryLoc").value;
+    var url="https://db.ygoprodeck.com/api/v7/cardinfo.php?name="+queryLoc
 
     $.get(url, callBackGetSuccess).done(function(){
         //alert("second success");
