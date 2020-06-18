22fa355e362385a09972c4db7b38436014aaae76
Thomas-Lefevre <thomas.lefevre4652@gmail.com>
2020-06-18 16:23:57 +0200

    Mise en page edit user et un seul admin Controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\DeckRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
@@ -16,4 +18,26 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+    /**
+     * @Route("/admin/deck", name="admin_deck")
+     */
+    public function indexDeck(DeckRepository $deckRepository)
+    {
+        return $this->render('admin_deck/adminDeck.html.twig', [
+            'decks' => $deckRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user_index", name="user_index", methods={"GET"})
+     */
+    public function indexUser(UserRepository $userRepository)
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+
 }
